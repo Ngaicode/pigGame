b98beb4ef0d8c39dfc8a4270e2e0ab558890919b
Ngaicode <Mjombakevin376@gmail.com>
2022-02-07 16:44:02 +0300

    implement roll dice functionality

--- starter/script.js
+++ starter/script.js
@@ -14,12 +14,38 @@
 const score0Element = document.querySelector("#score--0");
 // when selecting elements using their respective ID names ,we can alternatively use the  getElementById() method
 const score1Element = document.getElementById("score--1");
+const player0Currentscore = document.getElementById("current--0");
+const player1Currentscore = document.getElementById("current--1");
 const diceImageElement = document.querySelector(".dice");
+const rollDiceBtn = document.querySelector(".btn--roll");
+const newGameBtn = document.querySelector(".btn--new");
+const HoldCurrentScoreBtn = document.querySelector(".btn--hold");
 
 // ------------STARTING CONDITIONS------------
 // selecting the elements with the score and setting it back to zero at the start of the game
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 
+let currentScore = 0;
+
 // hiding the dice image at the begining of the game
 diceImageElement.classList.add("hidden");
+
+//implementing rolling dice functinality
+rollDiceBtn.addEventListener("click", function () {
+  // 1.generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2.display dice
+  diceImageElement.classList.remove("hidden");
+  //   dynamically updating the image of the rolled dice number
+  diceImageElement.src = `dice-${dice}.png`;
+  //   3.check if a player has rolled a 1;if true,switch to the next player
+  if (dice !== 1) {
+    //   add the rolled dice to the current score
+    currentScore += dice;
+    player0Currentscore.textContent = currentScore; //change this later
+  } else {
+    //   switch to next player if rolled a one
+  }
+});
