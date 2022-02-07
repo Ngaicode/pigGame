4d34435b5cae3a36af9af82e12969bd8029b815d
Ngaicode <Mjombakevin376@gmail.com>
2022-02-07 15:25:25 +0300

    first solution

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+
+// reinforcing knowledge is one of the most important thing that you can do to learn
+
+// AS ALWAYS ,WE START BY SELECTING THE ELEMENTS THAT WE WANT TO USE AND STORE THEM IN VARIABLES SO WE CAN EASILY USE THEM LATER
+// selecting the elements with the score and setting it back to zero
+const score = document.querySelectorAll(".score");
+for (let i = 0; i < score.length; i++) {
+  score[i].textContent = 0;
+}
