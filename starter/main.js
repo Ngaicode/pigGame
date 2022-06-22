ab240fb1d64e6cb5bff89557eb1566e9ad55f46a
Ngaicode <mjombakevin376@gmail.com>
2022-06-22 12:32:05 +0300

    created a function that displays the fetched pokemon data on the html

--- starter/main.js
+++ starter/main.js
@@ -19,3 +19,18 @@ fetch(apiUrl)
   })
   .then((pokemon) => generateHtml(pokemon))
   .catch((error) => console.error("Error:", error));
+
+// a function that displays the fetched pokemon data on the html
+const generateHtml = (data) => {
+  console.log(data);
+  const html = `
+        <div class="name">${data.name}</div>
+        <img src=${data.sprites.front_default}>
+        <div class="details">
+            <span>Height: ${data.height}</span>
+            <span>Weight: ${data.weight}</span>
+        </div>
+    `;
+  const pokemonDiv = document.querySelector(".pokemon");
+  pokemonDiv.innerHTML = html;
+};
