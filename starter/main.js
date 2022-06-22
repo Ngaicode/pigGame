686565debd2ab239001fcb85a834e942e07500ca
Ngaicode <mjombakevin376@gmail.com>
2022-06-22 12:14:36 +0300

    fetching data from the API about a specific pokemon

--- starter/main.js
+++ starter/main.js
@@ -6,3 +6,16 @@ const apiData = {
 };
 // deconstructing the apiData object
 const { url, type, id } = apiData;
+
+const apiUrl = `${url}${type}/${id}`;
+
+// fetching data from the  APi about a specific pokemon
+fetch(apiUrl)
+  .then((data) => {
+    if (data.ok) {
+      return data.json();
+    }
+    throw new Error("Response not ok.");
+  })
+  .then((pokemon) => generateHtml(pokemon))
+  .catch((error) => console.error("Error:", error));
